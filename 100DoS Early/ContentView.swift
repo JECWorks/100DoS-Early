8eab61b781fedd22a96f4bc7c4171eba70ef4bb5
JECWorks <36215363+JECWorks@users.noreply.github.com>
2024-06-06 21:37:51 -0400

    Initial Commit

---
+++ 100DoS Early/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  100DoS Early
+//
+//  Created by Jason Cox on 6/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 100DoS Early/_00DoS_EarlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  _00DoS_EarlyApp.swift
+//  100DoS Early
+//
+//  Created by Jason Cox on 6/6/24.
+//
+
+import SwiftUI
+
+@main
+struct _00DoS_EarlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
